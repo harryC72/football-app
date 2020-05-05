f7823cf8feb059de8f9a44e7574f5adfe5ad04db
HarryC72 <haraldcarlsten@yahoo.se>
2020-05-05 14:50:42 +0200

    Committing before implementing the first interface

---
+++ src/interfaces/match.ts
@@ -0,0 +1,40 @@
+interface HomeTeam {
+  id: number;
+  name: string;
+  wins: number;
+  draws: number;
+  losses: number;
+}
+
+interface AwayTeam {
+  id: number;
+  name: string;
+  wins: number;
+  draws: number;
+  losses: number;
+}
+
+interface Season {
+  startDate: string;
+  endDate: string;
+  currentMatchDay: 46;
+}
+
+export interface Match {
+  head2head: {
+    homeTeam: HomeTeam;
+    awayTeam: AwayTeam;
+  };
+  match: {
+    id: number;
+    status: string;
+    competition: {
+      id: number;
+      name: string;
+      area: {
+        name: string;
+      };
+    };
+  };
+  season: Season;
+}
